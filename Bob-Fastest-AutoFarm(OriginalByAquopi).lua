18e98dc828a61fad1cead6993606af5fef0cd815
Donjosx <159622061+Donjosx@users.noreply.github.com>
2024-07-25 13:03:08 +0700

    Update Bob-Fastest-AutoFarm(OriginalByAquopi).lua

--- Bob-Fastest-AutoFarm(OriginalByAquopi).lua
+++ Bob-Fastest-AutoFarm(OriginalByAquopi).lua
@@ -31,79 +31,92 @@ for i = 1, 75000 do
 	game:GetService("ReplicatedStorage").Duplicate:FireServer(unpack({[1] = true}))
 end
 wait(1)
-
+--[[ This script isn't mine but idk how to create one so i use this instead ]]--
 local AllIDs = {}
 local foundAnything = ""
 local actualHour = os.date("!*t").hour
 local Deleted = false
 local S_T = game:GetService("TeleportService")
 local S_H = game:GetService("HttpService")
+local Players = game:GetService("Players")
 
 local File = pcall(function()
-	AllIDs = S_H:JSONDecode(readfile("server-hop-temp.json"))
+    AllIDs = S_H:JSONDecode(readfile("server-hop-temp.json"))
 end)
 if not File then
-	table.insert(AllIDs, actualHour)
-	pcall(function()
-		writefile("server-hop-temp.json", S_H:JSONEncode(AllIDs))
-	end)
+    table.insert(AllIDs, actualHour)
+    pcall(function()
+        writefile("server-hop-temp.json", S_H:JSONEncode(AllIDs))
+    end)
+end
 
-end
 local function TPReturner(placeId)
-	local Site;
-	if foundAnything == "" then
-		Site = S_H:JSONDecode(game:HttpGet('https://games.roblox.com/v1/games/' .. placeId .. '/servers/Public?sortOrder=Asc&limit=100'))
-	else
-		Site = S_H:JSONDecode(game:HttpGet('https://games.roblox.com/v1/games/' .. placeId .. '/servers/Public?sortOrder=Asc&limit=100&cursor=' .. foundAnything))
-	end
-	local ID = ""
-	if Site.nextPageCursor and Site.nextPageCursor ~= "null" and Site.nextPageCursor ~= nil then
-		foundAnything = Site.nextPageCursor
-	end
-	local num = 0;
-	for i,v in pairs(Site.data) do
-		local Possible = true
-		ID = tostring(v.id)
-		if tonumber(v.maxPlayers) > tonumber(v.playing) then
-			for _,Existing in pairs(AllIDs) do
-				if num ~= 0 then
-					if ID == tostring(Existing) then
-						Possible = false
-					end
-				else
-					if tonumber(actualHour) ~= tonumber(Existing) then
-						local delFile = pcall(function()
-							delfile("server-hop-temp.json")
-							AllIDs = {}
-							table.insert(AllIDs, actualHour)
-						end)
-					end
-				end
-				num = num + 1
-			end
-			if Possible == true then
-				table.insert(AllIDs, ID)
-				wait()
-				pcall(function()
-					writefile("server-hop-temp.json", S_H:JSONEncode(AllIDs))
-					wait()
-					S_T:TeleportToPlaceInstance(placeId, ID, game.Players.LocalPlayer)
-				end)
-				wait(4)
-			end
-		end
-	end
+    local Site
+    if foundAnything == "" then
+        Site = S_H:JSONDecode(game:HttpGet('https://games.roblox.com/v1/games/' .. placeId .. '/servers/Public?sortOrder=Asc&limit=100'))
+    else
+        Site = S_H:JSONDecode(game:HttpGet('https://games.roblox.com/v1/games/' .. placeId .. '/servers/Public?sortOrder=Asc&limit=100&cursor=' .. foundAnything))
+    end
+
+    local ID = ""
+    if Site.nextPageCursor and Site.nextPageCursor ~= "null" and Site.nextPageCursor ~= nil then
+        foundAnything = Site.nextPageCursor
+    end
+
+    local num = 0
+    local foundServer = false  -- Biến để theo dõi trạng thái tìm thấy server
+
+    for i, v in pairs(Site.data) do
+        local Possible = true
+        ID = tostring(v.id)
+        if tonumber(v.maxPlayers) > tonumber(v.playing) then
+            for _, Existing in pairs(AllIDs) do
+                if num ~= 0 then
+                    if ID == tostring(Existing) then
+                        Possible = false
+                    end
+                else
+                    if tonumber(actualHour) ~= tonumber(Existing) then
+                        local delFile = pcall(function()
+                            delfile("server-hop-temp.json")
+                            AllIDs = {}
+                            table.insert(AllIDs, actualHour)
+                        end)
+                    end
+                end
+                num = num + 1
+            end
+            if Possible == true then
+                table.insert(AllIDs, ID)
+                foundServer = true  -- Đánh dấu là đã tìm thấy server
+                wait()
+                pcall(function()
+                    writefile("server-hop-temp.json", S_H:JSONEncode(AllIDs))
+                    wait()
+                    S_T:TeleportToPlaceInstance(placeId, ID, Players.LocalPlayer)
+                end)
+                wait(4)
+            end
+        end
+    end
+
+    -- if not found server
+    if not foundServer then
+        game:GetService("TeleportService"):Teleport(game.PlaceId)
+    end
 end
+
 local module = {}
 function module:Teleport(placeId)
-	while wait() do
-		pcall(function()
-			TPReturner(placeId)
-			if foundAnything ~= "" then
-				TPReturner(placeId)
-			end
-		end)
-	end
+    while wait() do
+        pcall(function()
+            TPReturner(placeId)
+            if foundAnything ~= "" then
+                TPReturner(placeId)
+            end
+        end)
+    end
 end
+
 return module
 module:Teleport(game.PlaceId)
