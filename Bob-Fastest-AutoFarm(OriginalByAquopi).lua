d3c4a7066c6e410af20996bf078e442b8dc931d5
Donjosx <159622061+Donjosx@users.noreply.github.com>
2024-07-25 14:30:36 +0700

    Update Bob-Fastest-AutoFarm(OriginalByAquopi).lua

--- Bob-Fastest-AutoFarm(OriginalByAquopi).lua
+++ Bob-Fastest-AutoFarm(OriginalByAquopi).lua
@@ -27,7 +27,7 @@ fireclickdetector(game:GetService("Workspace").Lobby.Replica.ClickDetector)
 task.wait(.3)
 firetouchinterest(game.Players.LocalPlayer.Character.HumanoidRootPart, workspace.Lobby.Teleport1, 0)
 task.wait(.1)
-for i = 1, 75000 do
+for i = 1, 75000000 do
 	game:GetService("ReplicatedStorage").Duplicate:FireServer(unpack({[1] = true}))
 end
 wait(2)
