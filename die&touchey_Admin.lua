3fa247e3b9c6cdc23ccd77cd58eeb111d89ba875
Donjosx <159622061+Donjosx@users.noreply.github.com>
2024-11-23 20:12:48 +0700

    Delete die&touchey_Admin.lua

--- die&touchey_Admin.lua
+++
@@ -1,5 +0,0 @@
-local Notification = loadstring(game:HttpGet("https://raw.githubusercontent.com/FilteringEnabled/FE/main/notificationtest"))();
- local Notify = Notification.Notify;
-Notify({Description = "This roblox script was removed because of someone post this script into executor search scripts" ; Title = "Script Removed!"; Duration = 5;});
-Notify({Description = "You can get new script at my video, link copied" ; Title = "Script Removed!"; Duration = 5;});
-setclipboard("https://youtu.be/KYEbdE5YiDk?si=7PExtX-3Tl--Bbe7")
